8a4b92a33dc33a5d618ad09a8b860c9188f00429
keerthidurai <keerthikadharmadurai@gmail.com>
2016-08-28 18:09:54 +0530

    Update and rename Reverse.java to Stringtoint.java

---
+++ Stringtoint.java
@@ -0,0 +1,12 @@
+class Strtoint
+{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    String str=s.nextLine();
+    int n=Integer.parseInt(str);
+    System.out.println(n);
+}
+}
+
+      
